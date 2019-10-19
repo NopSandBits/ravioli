c5f237531d956684bc209b6c6c4d9ba230cad85b
mchernosky <matt@chernosky.net>
2019-10-19 10:35:01 -0600

    Move report generation functions to their own file

--- ravioli/ravioli.py
+++ ravioli/ravioli.py
@@ -1,12 +1,12 @@
 import argparse
 import os
 import traceback
-from operator import itemgetter
 from pathlib import Path
 
 from ravioli.complexity import calculate_complexity
 from ravioli.global_finder import find_globals
 from ravioli.line_counter import count
+from ravioli.report_generator import report_all_functions, report_ksf_for_all_modules
 
 
 def run(filename, args):
@@ -36,83 +36,6 @@ def process_files(args, filename):
             if type(result) is ParsingError:
                 errors.append(result)
     return errors, results
-
-
-def report_all_functions(results, errors, args):
-
-    # Print globals.
-    print("-------------------------------------------------------------------------------")
-    print("Globals                                                                        ")
-    print("-------------------------------------------------------------------------------")
-    for result in results:
-        for g in result['globals_vars']:
-            print(result['filename'] + ':' + str(g.line_number) + ' ' + g.name)
-
-    # Assemble all the functions into one list.
-    functions = []
-    for result in results:
-        for f in result['functions']:
-            functions.append({'filename': result['filename'], 'line_number': f.line_number, 'name': f.name,
-                              'complexity': f.complexity})
-
-    # Sort the functions by complexity.
-    functions = sorted(functions, key=itemgetter('complexity'), reverse=True)
-
-    # Only display results above a threshold.
-    functions = [f for f in functions if f['complexity'] >= args.t]
-
-    # Print functions.
-    print("-------------------------------------------------------------------------------")
-    print("Functions                                                            complexity")
-    print("-------------------------------------------------------------------------------")
-    for f in functions:
-        remaining_filename = __print_wrapped_and_indented_string(f['filename'], 72)
-        print(remaining_filename + ':' + str(f['line_number']))
-
-        # Wrap long function names.
-        print("    ", end='')
-        remaining_function_name = __print_wrapped_and_indented_string(f['name'], 70)
-        print('{name:70} {complexity:3}'.format(name=remaining_function_name, complexity=f['complexity']))
-
-    if args.e:
-        print("-------------------------------------------------------------------------------")
-        print("Errors                                                                         ")
-        print("-------------------------------------------------------------------------------")
-        for e in errors:
-            print("*** ERROR PROCESSING: " + e.filename)
-            print(e.message)
-
-
-def report_ksf_for_all_modules(results, errors, args):
-
-    # Sort by spaghetti factor.
-    results = sorted(results, key=itemgetter('ksf'), reverse=True)
-
-    # Only display results above a threshold.
-    results = [r for r in results if r['ksf'] >= args.t]
-
-    print("-------------------------------------------------------------------------------")
-    print("File                                         complexity   globals   lines   ksf")
-    print("-------------------------------------------------------------------------------")
-    for result in results:
-        remaining_filename = __print_wrapped_and_indented_string(result['filename'], 50)
-        if remaining_filename != result['filename']:
-            # Some of the filename has already been printed.
-            print("{file:46}  {complexity:3}       {globals:3}   {loc:5}   {ksf:3}".format(
-                file=remaining_filename, complexity=result['max_scc'], globals=len(result['globals_vars']),
-                loc=result['loc'], ksf=result['ksf']))
-        else:
-            print("{file:50}  {complexity:3}       {globals:3}   {loc:5}   {ksf:3}".format(
-                file=remaining_filename, complexity=result['max_scc'], globals=len(result['globals_vars']),
-                loc=result['loc'], ksf=result['ksf']))
-
-    if args.e:
-        print("-------------------------------------------------------------------------------")
-        print("Errors                                                                         ")
-        print("-------------------------------------------------------------------------------")
-        for e in errors:
-            print("*** ERROR PROCESSING: " + e.filename)
-            print(e.message)
 
 
 class ParsingError:
@@ -173,26 +96,6 @@ def main():
 
 def __file_result_is_valid(result):
     return type(result) is not ParsingError
-
-
-def __print_wrapped_and_indented_string(str, width):
-    remaining_str = str
-    space_for_str = width
-    while len(remaining_str) > space_for_str:
-        if len(remaining_str) == len(str):
-            # This is the first line.
-            print(remaining_str[:space_for_str])
-            remaining_str = remaining_str[space_for_str:]
-            space_for_str = width - 4
-        else:
-            print("    ", end='')
-            print(remaining_str[:space_for_str])
-            remaining_str = remaining_str[space_for_str:]
-    if remaining_str != str:
-        # We wrapped some of the string. Print an indent so that the next
-        # print is indented.
-        print("    ", end='')
-    return remaining_str
 
 
 if __name__ == "__main__":
---
+++ ravioli/report_generator.py
@@ -0,0 +1,98 @@
+from operator import itemgetter
+
+
+def report_all_functions(results, errors, args):
+
+    # Print globals.
+    print("-------------------------------------------------------------------------------")
+    print("Globals                                                                        ")
+    print("-------------------------------------------------------------------------------")
+    for result in results:
+        for g in result['globals_vars']:
+            print(result['filename'] + ':' + str(g.line_number) + ' ' + g.name)
+
+    # Assemble all the functions into one list.
+    functions = []
+    for result in results:
+        for f in result['functions']:
+            functions.append({'filename': result['filename'], 'line_number': f.line_number, 'name': f.name,
+                              'complexity': f.complexity})
+
+    # Sort the functions by complexity.
+    functions = sorted(functions, key=itemgetter('complexity'), reverse=True)
+
+    # Only display results above a threshold.
+    functions = [f for f in functions if f['complexity'] >= args.t]
+
+    # Print functions.
+    print("-------------------------------------------------------------------------------")
+    print("Functions                                                            complexity")
+    print("-------------------------------------------------------------------------------")
+    for f in functions:
+        remaining_filename = __print_wrapped_and_indented_string(f['filename'], 72)
+        print(remaining_filename + ':' + str(f['line_number']))
+
+        # Wrap long function names.
+        print("    ", end='')
+        remaining_function_name = __print_wrapped_and_indented_string(f['name'], 70)
+        print('{name:70} {complexity:3}'.format(name=remaining_function_name, complexity=f['complexity']))
+
+    if args.e:
+        print("-------------------------------------------------------------------------------")
+        print("Errors                                                                         ")
+        print("-------------------------------------------------------------------------------")
+        for e in errors:
+            print("*** ERROR PROCESSING: " + e.filename)
+            print(e.message)
+
+
+def report_ksf_for_all_modules(results, errors, args):
+
+    # Sort by spaghetti factor.
+    results = sorted(results, key=itemgetter('ksf'), reverse=True)
+
+    # Only display results above a threshold.
+    results = [r for r in results if r['ksf'] >= args.t]
+
+    print("-------------------------------------------------------------------------------")
+    print("File                                         complexity   globals   lines   ksf")
+    print("-------------------------------------------------------------------------------")
+    for result in results:
+        remaining_filename = __print_wrapped_and_indented_string(result['filename'], 50)
+        if remaining_filename != result['filename']:
+            # Some of the filename has already been printed.
+            print("{file:46}  {complexity:3}       {globals:3}   {loc:5}   {ksf:3}".format(
+                file=remaining_filename, complexity=result['max_scc'], globals=len(result['globals_vars']),
+                loc=result['loc'], ksf=result['ksf']))
+        else:
+            print("{file:50}  {complexity:3}       {globals:3}   {loc:5}   {ksf:3}".format(
+                file=remaining_filename, complexity=result['max_scc'], globals=len(result['globals_vars']),
+                loc=result['loc'], ksf=result['ksf']))
+
+    if args.e:
+        print("-------------------------------------------------------------------------------")
+        print("Errors                                                                         ")
+        print("-------------------------------------------------------------------------------")
+        for e in errors:
+            print("*** ERROR PROCESSING: " + e.filename)
+            print(e.message)
+
+
+def __print_wrapped_and_indented_string(str, width):
+    remaining_str = str
+    space_for_str = width
+    while len(remaining_str) > space_for_str:
+        if len(remaining_str) == len(str):
+            # This is the first line.
+            print(remaining_str[:space_for_str])
+            remaining_str = remaining_str[space_for_str:]
+            space_for_str = width - 4
+        else:
+            print("    ", end='')
+            print(remaining_str[:space_for_str])
+            remaining_str = remaining_str[space_for_str:]
+    if remaining_str != str:
+        # We wrapped some of the string. Print an indent so that the next
+        # print is indented.
+        print("    ", end='')
+    return remaining_str
